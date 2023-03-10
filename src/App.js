c55050b78083ef9259f44cf0f40a53dbd02b9e80
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-10 13:56:10 -0500

    added header and button to switch page urls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Shelf from './images/shelf.jpg';
+import { Link } from 'react-router-dom'
+//import ShopPage from './ShopPage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='header'>
+        <h1>Fruits</h1>
+        <h2>Shop</h2>
+        <h2>Cart</h2>
+      </div>
+      <div className='info'>
+        <div className='slogan'>Check out this wide assortment of fruits! <img class='shelf' alt='' src={Shelf}></img></div>
+        <Link to='/shop' className='shopBtn'>SHOP</Link>
+      </div>
     </div>
   );
 }
