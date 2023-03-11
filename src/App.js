8dea89e6726de42778db8e6ff95219b17858ac14
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-10 21:44:36 -0500

    adds cart button functionallity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import Shelf from './images/shelf.jpg';
 import { Link } from 'react-router-dom'
-//import ShopPage from './ShopPage.js';
+import { useState } from 'react';
+// import { useEffect } from 'react';
+import React from 'react';
+// import ShopPage from './ShopPage.js';
 
 function App() {
+  let [shown, nextVal] = useState(0);
+
+  function ShowCart() {
+      const cartDisplay = document.querySelector('.cart');
+      if (shown === 0) {
+        cartDisplay.style.display = 'block';
+        nextVal(prev => prev = 1);
+      }
+      else {
+        nextVal(prev => prev = 0);
+        cartDisplay.style.display = 'none';
+      }
+  }
+
   return (
     <div>
+      <div className='cart'>
+        <div>hello</div>
+      </div>
       <div className='header'>
-        <h2>Fruits</h2>
-        <Link className='homeShop' to='/shop'> <h2>Shop</h2></Link>
-        <h2>Cart</h2>
+        <h3>Home</h3>
+        <Link className='homeShop' to='/shop'> <h3>Shop</h3></Link>
+        <h3 className='cartBtn' onClick={ShowCart}>Cart</h3>
       </div>
       <div className='info'>
         <div className='slogan'>Check out this wide assortment of fruits! <img className='shelf' alt='' src={Shelf}></img></div>
--- src/ShopPage.js
+++ src/ShopPage.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 function shopContents() {
     return (
         <div>
-            <div className='shop-header'>
-                <Link to='/' className='BackHome'>Home</Link>
-                <div>hello</div>
+            <div className='header'>
+                <h3>Catalog</h3>
+                <Link to='/' className='BackHome'><h3>Home</h3></Link>
+                <h3>Cart</h3>
             </div>
         </div>
     )
