34bb5e40fbf91fe9ac041c5438462aaf75765b67
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-17 23:27:04 -0400

    simplified home page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
 import Shelf from './images/shelf.jpg';
 import { Link } from 'react-router-dom'
-import { useState } from 'react';
-import Watermelon from './images/watermelon.png';
-// import { useEffect } from 'react';
 import React from 'react';
-// import ShopPage from './ShopPage.js';
-// import ShowCart from './ShowCart';
 
 export default function App() {
-  let [toggleHeader, setToggleHeader] = useState(0);
-
-  function ShowCart() {
-      const cartDisplay = document.querySelector('.cart');
-      if (toggleHeader === 0) {
-        cartDisplay.style.display = 'block';
-        setToggleHeader(prev => prev = 1);
-      }
-      else {
-        setToggleHeader(prev => prev = 0);
-        cartDisplay.style.display = 'none';
-      }
-  }
-
   return (
-    // {ShowCart},
     <div>
       <div className='cart'>
 
@@ -32,7 +12,6 @@ export default function App() {
       <div className='header'>
         <h3 style={{color: 'black'}}>Home</h3>
         <Link className='homeShop' to='/shop'> <h3>Shop</h3></Link>
-        <h3 className='cartBtn' onClick={ShowCart}>Cart</h3>
       </div>
       <div className='info'>
         <img className='shelf' alt='' src={Shelf}></img>
@@ -41,20 +20,4 @@ export default function App() {
       </div>
     </div>
   );
-}
-
-export function displayMelonCart(props) {
-  const cartDisplay = document.querySelector('.cart');
-  let img = Watermelon;
-  let txt = 'Watermelon';
-
-  const watermelonImg = document.createElement('img');
-  const watermelonTxt = document.createElement('div');
-  watermelonImg.src = img;
-  watermelonTxt.textContent = txt;
-  watermelonImg.classList.add('cartImg');
-  // watermelonTxt.classList.add('fruitDescription');
-  cartDisplay.appendChild(watermelonImg);
-  // cartDisplay.appendChild(watermelonTxt);
-  cartDisplay.style.display = 'flex';
 }
