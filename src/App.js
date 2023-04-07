6d8b499e9b123eb9a4cdb6dd921c54844d2df378
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-04-07 00:25:33 -0400

    switched home page header name to Catalog

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ export default function App() {
       </div>
       <div className='header'>
         <h3 style={{color: 'black'}}>Home</h3>
-        <Link className='homeShop' to='/shop'> <h3>Shop</h3></Link>
+        <Link className='homeShop' to='/shop'> <h3>Catalog</h3></Link>
       </div>
       <div className='info'>
         <img className='shelf' alt='' src={Shelf}></img>
         <div className='slogan'>Check out this wide assortment of fruits!</div>
-        <Link to='/shop' className='shopBtn'>Catalog</Link>
+        <Link to='/shop' className='shopBtn'>SHOP</Link>
       </div>
     </div>
   );
